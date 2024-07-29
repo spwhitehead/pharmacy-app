e425dcd727ba3716dac8324b3cb756df48b21d07
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-07-29 09:52:25 -0600

    Initial commit, change Logo

---
+++ pharmacy-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Logo from './assets/DixieTechPharmacyAppLogo.svg'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={Logo} alt='Dixie Tech Pharmacy App Logo' />
+    </div>
+    </>
+  )
+}
+
+export default App
